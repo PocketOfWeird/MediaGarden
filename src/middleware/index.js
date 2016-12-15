6220e7c17b2c797a248558b5223dd21ad6b82fe7
Pocket of Weird <hartzlernathan@gmail.com>
2016-12-15 09:49:20 -0600

    Throttled search actions

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,6 +1,8 @@
 import { applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import createLogger from 'redux-logger'
+import throttleActions from 'redux-throttle-actions'
+import { SERVER_SEARCH, SERVER_STATE } from '../actions'
 import localStore from './local'
 import on from './on'
 import search from './search'
@@ -12,10 +14,12 @@ import view from './view'
 
 
 const logger = createLogger({collapsed: true})
+const throttler = throttleActions([SERVER_SEARCH, SERVER_STATE], 500)
 
 const configureMiddleware = () => {
   return applyMiddleware(
     thunk,
+    throttler,
     localStore,
     on,
     search,
